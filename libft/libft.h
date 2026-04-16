8aec73fb8d2b7f3a736d8dcd9b81ac76707bdf39
Eric <eric.trignau@gmail.com>
2026-04-16 11:24:35 +0200

    push libft + structre and init

---
+++ libft/libft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eric <eric@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/16 11:15:05 by eric              #+#    #+#             */
+/*   Updated: 2026/04/16 11:20:35 by eric             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+#define LIBFT_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int		ft_atoi(const char *str);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+
+#endif
---
+++ mandatory/init.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eric <eric@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/15 13:53:38 by eric              #+#    #+#             */
+/*   Updated: 2026/04/16 11:14:29 by eric             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "malcolm.h"
+
+t_entry *add_entry(t_entry **table, uint8_t *ip, uint8_t *mac)
+{
+	t_entry *new;
+
+	new = malloc(sizeof(t_entry));
+	if (!new)
+		return (NULL);
+	ft_memcpy(new->ip, ip, 4);
+	ft_memcpy(new->mac, mac, 6);
+	new->next = *table;
+	*table = new;
+	return (new);
+}
